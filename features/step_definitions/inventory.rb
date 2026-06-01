bffbbbb436b179403e8bb7cc7b4a76800db676c8
Mauricio <mauricitogarron@gmail.com>
2026-05-31 22:55:06 -0400

    refact: inventario actualizado

--- features/step_definitions/inventory.rb
+++ features/step_definitions/inventory.rb
@@ -1,13 +1,54 @@
-Before do
+INVENTORY_USER = 'standard_user'
+INVENTORY_PASSWORD = 'secret_sauce'
+INVENTORY_URL = 'https://www.saucedemo.com/inventory.html'
+
+Before('@inventory') do
   begin
     Capybara.current_session.driver.quit
   rescue
   end
+
   Capybara.reset_sessions!
 end
 
+def login_as_inventory_user
+  visit('https://www.saucedemo.com/')
+
+  fill_in 'user-name', with: INVENTORY_USER
+  fill_in 'password', with: INVENTORY_PASSWORD
+  click_button 'login-button'
+
+  expect(page).to have_current_path('/inventory.html', ignore_query: true)
+  expect(page).to have_css('.inventory_list')
+end
+
+def sort_inventory_by(sort_option)
+  find('.product_sort_container').select(sort_option)
+end
+
+def first_inventory_item_name
+  all('.inventory_item_name').first.text
+end
+
+def first_inventory_item_price
+  all('.inventory_item_price').first.text
+end
+
+def first_inventory_item_value(field)
+  case field
+  when 'name'
+    first_inventory_item_name
+
+  when 'price'
+    first_inventory_item_price
+
+  else
+    raise "Unsupported inventory field: #{field}"
+  end
+end
+
 When('I visit the inventory page directly') do
-  visit('https://www.saucedemo.com/inventory.html')
+  visit(INVENTORY_URL)
 end
 
 Then('I should remain on the login page after direct inventory access') do
@@ -20,25 +61,13 @@ Then('I should see the inventory access denied message') do
 end
 
 Given('I am logged in on the SauceDemo inventory page') do
-  visit('https://www.saucedemo.com/')
-  fill_in 'user-name', with: 'standard_user'
-  fill_in 'password', with: 'secret_sauce'
-  click_button 'login-button'
-
-  expect(page).to have_current_path('/inventory.html', ignore_query: true)
-  expect(page).to have_css('.inventory_list')
+  login_as_inventory_user
 end
 
 When('I sort inventory by {string}') do |sort_option|
-  find('.product_sort_container').select(sort_option)
+  sort_inventory_by(sort_option)
 end
 
-Then('the first inventory product should be {string}') do |expected_product_name|
-  first_product_name = all('.inventory_item_name').first.text
-  expect(first_product_name).to eq(expected_product_name)
-end
-
-Then('the first inventory product price should be {string}') do |expected_price|
-  first_product_price = all('.inventory_item_price').first.text
-  expect(first_product_price).to eq(expected_price)
+Then('the first inventory item should show {string} in the {string} field') do |expected_value, field|
+  expect(first_inventory_item_value(field)).to eq(expected_value)
 end
