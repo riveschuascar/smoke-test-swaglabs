2f4ad4bfbfe2409f1f8bc28139685add64cca6fb
unknown <huascar.rivero@ucb.edu.bo>
2026-05-31 21:51:22 -0400

    refact: hacer el BDD mas imperativo

--- features/step_definitions/logout.rb
+++ features/step_definitions/logout.rb
@@ -1,16 +1,40 @@
-Given('I am logged in as a standard user') do
+Given('I navigate to the login page') do
   visit('https://www.saucedemo.com/')
-  fill_in 'user-name', with: 'standard_user'
-  fill_in 'password', with: 'secret_sauce'
+end
 
+Given('I enter the credentials') do |table|
+  data = table.hashes.first
+  fill_in 'user-name', with: data['user']
+  fill_in 'password', with: data['password']
+end
+
+Given('I click the Login button') do
   click_button 'login-button'
 end
 
-When('I log out') do
+Given('I verify that the Products page is displayed') do
+  expect(page).to have_text('Products')
+
+  inventory_items = all('.inventory_item')
+  expect(inventory_items.count).to eq(6)
+end
+
+When('I click the hamburger menu button') do
   find('#react-burger-menu-btn').click
+end
+
+When('I click the Logout button') do
   find('#logout_sidebar_link').click
 end
 
 Then('I should be redirected to the login page') do
-  expect(page).to have_current_path('https://www.saucedemo.com/', ignore_query: true)
+  expect(page).to have_current_path('https://www.saucedemo.com/')
+  expect(page).to have_field('user-name')
+  expect(page).to have_field('password')
+end
+
+Then('I should not have access to the Products page') do
+  visit('https://www.saucedemo.com/inventory.html')
+  expect(page).to have_current_path('https://www.saucedemo.com/')
+  expect(page).to have_text("Epic sadface: You can only access '/inventory.html' when you are logged in.")
 end
--- logout.feature
+++ logout.feature
@@ -1,9 +1,17 @@
+@smoke
 Feature: Logout
   As a user
   I want to be able to log out from my account
   So that my information remains secure
 
 Scenario: Successful logout
-  Given I am logged in as a standard user
-  When I log out
+  Given I navigate to the login page
+  And I enter the credentials
+  | user          | password     |
+  | standard_user | secret_sauce |
+  And I click the Login button
+  And I verify that the Products page is displayed
+  When I click the hamburger menu button
+  And I click the Logout button
   Then I should be redirected to the login page
+  And I should not have access to the Products page
