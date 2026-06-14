a905e6e7643c305025e43ee1032a0f10caeb12b5
unknown <huascar.rivero@ucb.edu.bo>
2026-06-14 19:32:55 -0400

    refact: implementacion de POM

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,9 @@
 After do
   Capybara.reset_sessions!
 end
+
+Before('@details') do
+  @login_page = LoginPage.new
+  @inventory_page = InventoryPage.new
+  @product_page = ProductDetailPage.new
+end
